87ac18b7c8fac4ba8b3c215871ec66036a506493
chesco502 <97119222+chesco502@users.noreply.github.com>
2023-07-16 22:01:02 -0300

    Create Calculadora_Mutex_π.c

---
+++ Calculadora_Mutex_π.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <pthread.h>
+
+#define quantidade_de_threads 4
+
+double sum = 0.0;
+int n = 1000;
+int thread_count = quantidade_de_threads ;
+pthread_mutex_t mutex;
+
+void *Thread_sum(void *rank) {
+    pthread_mutex_lock(&mutex);
+    long my_rank = (long) rank;
+    double factor;
+    long long i;
+    long long my_n = n/thread_count;
+    long long my_first_i = my_n * my_rank;
+    long long my_last_i = my_first_i + my_n;
+    if (my_first_i == 0) {
+        factor = 1.0;
+    } else {
+        factor = -1.0;
+    }
+    
+    for (i = my_first_i; i < my_last_i; i++, factor = -factor) {
+        double my_sum = factor / (2.0 * i + 1.0);
+        
+        sum += my_sum;
+        
+    }
+    
+    printf("thread %li valor de pi :%f \n",my_rank,sum*4);
+    pthread_mutex_unlock(&mutex);
+    return NULL;
+}
+
+int main() {
+    pthread_t threads[quantidade_de_threads ];
+    long i;
+    pthread_mutex_init(&mutex, NULL);
+    for (i = 0; i < thread_count; i++) {
+        pthread_create(&threads[i], NULL, Thread_sum, (void *) i);
+    }
+    for (i = 0; i < thread_count; i++) {
+        pthread_join(threads[i], NULL);
+    }
+    pthread_mutex_destroy(&mutex);
+    double pi = 4 * sum;
+    printf("Pi aproximado com N %d e com %i threads : %.10f\n", n, thread_count, pi);
+    return 0;
+}
